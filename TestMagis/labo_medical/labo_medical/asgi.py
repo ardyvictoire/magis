9589a1fd93afcf77bc3509b3f85450b3d3a70596
ardyvitoire <ardyvictoire@gmail.com>
2024-10-22 12:22:59 +0200

    modifacate

--- TestMagis/labo_medical/labo_medical/asgi.py
+++ TestMagis/labo_medical/labo_medical/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'labo_medical.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "labo_medical.settings.settings")
 
 application = get_asgi_application()
--- TestMagis/labo_medical/labo_medical/wsgi.py
+++ TestMagis/labo_medical/labo_medical/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'labo_medical.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "labo_medical.settings.settings")
 
 application = get_wsgi_application()
